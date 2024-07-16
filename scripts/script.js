e64c8fd0b4e8ec9d6b267bb3d172010eb8ecd01c
BiliOrange <90908765+orangeZSCB@users.noreply.github.com>
2024-07-16 20:07:47 +0800

    Update my main-page
    Looks more beautiful LOL

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// Typed.js
+  const options = {
+    strings: [
+          "是橙子🍊",
+          "来自橙星🍊",
+          "是一个中学生🤓",
+          "是一个啥也不会的巴卡马卡🤔",
+          "是......?"
+    ],
+    typeSpeed: 100,
+    loop: true,
+    startDelay: 500,
+  };
+
+  const typed = new Typed('#typed', options);
+
+// 随机年龄
+// 使用 Fitten Code 人工智能编写
+document.addEventListener('DOMContentLoaded', function () {
+  let timer;  // 声明一个变量来保存定时器ID
+  function updateRandomAge() {
+    var randomAge = Math.floor(Math.random() * 90) + 10;
+    document.getElementById("randomAge").textContent = randomAge;
+    clearTimeout(timer);  // 清除之前的定时器
+    timer = setTimeout(updateRandomAge, 50);  // 更新定时器
+  }
+
+  updateRandomAge();  // 首次调用
+});
